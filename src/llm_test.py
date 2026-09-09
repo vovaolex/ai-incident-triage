2f63041a552ead32c06167dd19cb3510e1f9d56a
vovaolex <vovaolex123@gmail.com>
2026-09-10 01:04:04 +0200

    Save enriched AI triage results

--- src/llm_test.py
+++ src/llm_test.py
@@ -22,6 +22,8 @@ class IncidentAnalysis(BaseModel):
 with open("data/incidents.json", "r") as file:
     incidents = json.load(file)
 
+enriched_incidents = []
+
 for incident in incidents:
     # call LLM
     response = client.responses.parse(
@@ -50,12 +52,24 @@ for incident in incidents:
 )
     # get result
     result = response.output_parsed
-    # print incident number
-    print(f"INC: {incident["number"]}")
-    # print category
-    print(f"Category: {result.category}")
-    # print summary
-    print(f"Summary: {result.summary}")
-    # loop through recommended actions
-    for action in result.recommended_actions:
-        print(f"- {action}")
+    ai_analysis = {
+    "ai_category": result.category,
+    "ai_summary": result.summary,
+    "probable_cause": result.probable_cause,
+    "recommended_actions": result.recommended_actions
+}
+    enriched_incident = {
+    "number": incident["number"],
+    "short_description": incident["short_description"],
+    "description": incident["description"],
+    "impact": incident["impact"],
+    "urgency": incident["urgency"],
+    **ai_analysis
+}
+    enriched_incidents.append(enriched_incident)
+    print(f"Processed {incident['number']}")
+
+with open("data/ai_triaged_incidents.json", "w") as file:
+    json.dump(enriched_incidents, file, indent=4)
+
+print("AI triage completed and saved.")
